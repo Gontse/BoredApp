48af56ab8597c52610c7014f5a4ad30f173cd5cc
Gontse Ranoto <gontse.ranoto@absa.africa>
2023-01-28 17:14:43 +0200

    Initial Commit

---
+++ BoredApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoredApp
+//
+//  Created by Gontse Ranoto (ZA) on 2023/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
